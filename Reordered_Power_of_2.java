fd6943f74a766192aa706a9e2ac461fee4d00e05
Welyab Paula <welyab.exe@gmail.com>
2019-01-23 10:32:55 -0300

    Implement_Magic_Dictionary.java

--- Reordered_Power_of_2.java
+++ Reordered_Power_of_2.java
@@ -1,17 +1,36 @@
 // https://leetcode.com/problems/reordered-power-of-2/
 public class Reordered_Power_of_2 {
+
 	public boolean reorderedPowerOf2(int N) {
 		char digits[] = Integer.toString(N).toCharArray();
-		boolean memory[] = new boolean[digits.length];
-    }
-    
-    public boolean isPowerOfTwo(int n) {
-        while(n>1&&n%2==0) n/=2;
-		return n==1;
+		return permuteAndCheck(digits, digits.length);
 	}
-	
-	public static void main(String [] args) {
-		Reordered_Power_of_2 p = new Reordered_Power_of_2();
-		System.out.println(p.reorderedPowerOf2(24));
+
+	public boolean permuteAndCheck(char[] digits, int n) {
+		if (n == 1 && digits[0] == '0') {
+			return false;
+
+		} else if (n == 1) {
+			return isPowerOfTwo(Integer.parseInt(new String(digits)));
+		}
+
+		for (int i = 0; i < n; i++) {
+			boolean b = permuteAndCheck(digits, n - 1);
+			if (b) {
+				return true;
+			}
+			int a = n % 2 == 0 ? i : 0;
+			char c = digits[a];
+			digits[a] = digits[n - 1];
+			digits[n - 1] = c;
+		}
+		return false;
+	}
+
+	public boolean isPowerOfTwo(int n) {
+		while (n > 1 && n % 2 == 0) {
+			n /= 2;
+		}
+		return n == 1;
 	}
 }
